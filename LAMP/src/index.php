b9a799b9bcf87484b10a9cf84b30b0264405b37d
Alberto Casanova <58746126+JudahAvadom@users.noreply.github.com>
2022-11-29 17:38:31 -0500

    Docker

---
+++ LAMP/src/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/loginBox.css">
+</head>
+<body>
+    <div class="navbar">
+        <nav>
+            <ul>
+                <li><h3>Welcome</h3></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="login__box">
+        <form action="">
+            <input type="text" name="" id="" placeholder="Username" >
+            <input type="password" name="" id="" placeholder="Paswword" >
+        </form>
+    </div>
+</body>
+</html>
